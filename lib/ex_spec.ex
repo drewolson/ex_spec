1c646f13fe5d7e4aedc2a4f508e44bb5d6e555de
drew <drew@drewolson.org>
2014-12-31 13:01:07 -0600

    Add tasks for generating docs.

--- lib/ex_spec.ex
+++ lib/ex_spec.ex
@@ -1,4 +1,45 @@
 defmodule ExSpec do
+  @moduledoc """
+  ExSpec is a simple wrapper around ExUnit that adds Rspec-style macros. Specifically, it adds `describe`, `context` and `it`.
+
+  While it takes inspiration from Rspec, ExSpec is significantly simplier. The `describe` and `context` macros have only two functions:
+
+  1. Aid test organization
+  2. Prepend to the message of any `it` defined within their do blocks
+
+  Furthermore, `describe` and `context` are aliases and function identically.
+
+  The `it` macro is identical to `ExUnit.Case.test` except that it is aware of the messages of its surrounding `describe` and `context` blocks.
+
+  Other than the functionality described above, ExSpec is just ExUnit. When `use`ing `ExSpec`, any options provided will be passed to `ExUnit.Case` (e.g. `async: true`).
+
+  A simple example is shown below. For more examples, see the tests.
+
+  ## Example
+
+      defmodule PersonTest do
+        use ExSpec, async: true
+
+        describe "#name" do
+          context "with first and last name" do
+            it "joins the names with a space" do
+              drew = %Person{first_name: "Drew", last_name: "Olson"}
+
+              assert Person.name(drew) == "Drew Olson"
+            end
+          end
+
+          context "with only a first name" do
+            it "returns the first name" do
+              drew = %Person{first_name: "Drew", last_name: nil}
+
+              assert Person.name(drew) == "Drew"
+            end
+          end
+        end
+      end
+  """
+
   defmacro __using__(options) do
     quote do
       import ExSpec
--- mix.exs
+++ mix.exs
@@ -2,12 +2,16 @@ defmodule ExSpec.Mixfile do
   use Mix.Project
 
   def project do
-    [app: :ex_spec,
-     version: "0.3.0",
-     elixir: "~> 1.0",
-     package: package,
-     description: "BDD-like syntax for ExUnit",
-     deps: deps]
+    [
+      app: :ex_spec,
+      version: "0.3.0",
+      elixir: "~> 1.0",
+      package: package,
+      description: "BDD-like syntax for ExUnit",
+      source_url: "https://github.com/drewolson/ex_spec",
+      homepage_url: "https://hex.pm/packages/ex_spec",
+      deps: deps
+    ]
   end
 
   def application do
@@ -15,7 +19,10 @@ defmodule ExSpec.Mixfile do
   end
 
   defp deps do
-    []
+    [
+      {:earmark, ">= 0.0.0", only: :dev},
+      {:ex_doc, "~> 0.6", only: :dev}
+    ]
   end
 
   defp package do
