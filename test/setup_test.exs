5461321a857721ac5cabd945c2cfa3b37b48fdd9
apeckham <apeckham@gmail.com>
2015-03-07 22:53:47 -0800

    add failing test to show problem in setup blocks

--- test/setup_test.exs
+++ test/setup_test.exs
@@ -9,7 +9,19 @@ defmodule SetupTest do
     context "are handled" do
       it "correctly", context do
         assert context[:foo] == "bar"
+        assert context[:baz] == nil
       end
+    end
+  end
+
+  describe "setups within blocks" do
+    setup do
+      {:ok, [baz: "qux"]}
+    end
+  
+    it "runs setup only in this block", context do
+      assert context[:foo] == "bar"
+      assert context[:baz] == "qux"
     end
   end
 end
